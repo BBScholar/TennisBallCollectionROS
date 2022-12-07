98881ea89056ceaf345b2956f15c59eb2083c8cb
Ben Scholar <scholar@case.edu>
2022-12-06 19:47:49 -0500

    working well

--- leo_autonomy/src/collect.py
+++ leo_autonomy/src/collect.py
@@ -7,7 +7,7 @@ import math
 
 import actionlib 
 
-from geometry_msgs.msg import Pose, PoseArray, PoseStamped
+from geometry_msgs.msg import Pose, PoseArray, PoseStamped, Twist
 from std_msgs.msg import Bool, Int32
 from std_srvs.srv import SetBool
 
@@ -37,15 +37,17 @@ class CollectNode:
             
 
         self.home_coords = Pose()
-        self.home_coords.position.x = 0 
-        self.home_coords.position.y = 0 
+        self.home_coords.position.x = 1.0
+        self.home_coords.position.y = 1.0
         self.home_coords.position.z = 0
+        self.home_coords.orientation.w = 1.0
         # self.home_coords.orientation.z = 1.0
 
         # patrol variables
         self.patrol = []
-        self.saved_postion = None
+        self.saved_position = None
         self.move_base_seq = 0
+        self.dumps = 0
 
         self.seek_path_done = False
 
@@ -58,6 +60,7 @@ class CollectNode:
 
         # publishers
         self.total_balls_pub = rospy.Publisher("collected/total_balls_collected", Int32, queue_size=32)
+        self.velocity_pub = rospy.Publisher("diff_drive/cmd_vel", Twist, queue_size=32)
         
         # subscribers
         self.vacuum_state_sub = rospy.Subscriber("vacuum/state", Bool, self.vac_state_cb, queue_size=32)
@@ -114,8 +117,9 @@ class CollectNode:
 
     def move_seek_done(self, state, result):
         rospy.loginfo(f"Done with seeking path, with code {state}")
-        if state == 3:
-            self.seek_path_done = True
+        self.seek_path_done = True
+        if state != 3:
+            rospy.logwarn("Seeking path unsucessfull")
 
     def generate_patrol(self):
         p1 = Pose()
@@ -148,30 +152,35 @@ class CollectNode:
         self.patrol.append(p4)
 
 
-    def save_position(self):
+    def get_current_position(self):
         odom_pose = self.odom.pose.pose
 
         odom_map_t = self.get_transform("map", "odom")
 
         if odom_map_t is None:
             rospy.logerr("Cannot get odom transform")
-            return 
+            return None
 
         pose_stamped = PoseStamped()
         pose_stamped.pose = odom_pose 
 
         map_pose = tf2_geometry_msgs.do_transform_pose(pose_stamped, odom_map_t)
-        
+
+        return map_pose.pose
+
+
+    def save_position(self):
+        map_pose = self.get_current_position() 
         rospy.loginfo(f"Saved position: {str(map_pose)}")
-        self.save_position = map_pose.pose
+        self.saved_position = map_pose
 
     def cancel_all_move_goals(self):
         self.move_base_as.cancel_all_goals()
 
     def go_to_saved(self):
-        if self.save_position is not None:
-            self.add_move_goal(self.save_position, False)
-            self.save_position = None
+        if self.saved_position is not None:
+            self.add_move_goal(self.saved_position, False)
+            self.saved_position = None
 
 
     def add_move_goal(self, pose, enqueue_patrol=True):
@@ -196,6 +205,13 @@ class CollectNode:
             self.move_base_as.send_goal(goal.goal, done_cb=self.move_base_done)
         else:
             self.move_base_as.send_goal(goal.goal, done_cb=self.move_seek_done)
+
+    def pose_dist(self, a, b):
+        x = a.position.x - b.position.x 
+        y = a.position.y - b.position.y 
+        dist = math.hypot(x, y)
+        return dist
+
 
 
     def get_transform(self, target, source):
@@ -235,8 +251,8 @@ class CollectNode:
         vacuum_pose = tf2_geometry_msgs.do_transform_pose(pose_stamped, base_vacuum_t)
 
 
-        x = vacuum_pose.pose.position.x
-        y = vacuum_pose.pose.position.y
+        x = pose.position.x
+        y = pose.position.y
 
         at = math.atan2(y, x)
 
@@ -253,26 +269,73 @@ class CollectNode:
         # ARCTAN stuff here
         return map_pose.pose
 
+    def get_closest_ball(self):
+        # t = self.get_transform("map", self.visible_balls.header.frame_id)
+        # if t is None:
+        #     rospy.logerr("Cannot get transform for closest ball")
+        #     return None
+        current_location = self.get_current_position()
+        closest_pose = self.generate_ball_pose(self.visible_balls.poses[0], self.visible_balls.header.frame_id)
+        shortest_dist = self.pose_dist(current_location, closest_pose)
+        for i in range(1, len(self.visible_balls.poses)):
+            new_pose = self.generate_ball_pose(self.visible_balls.poses[i], self.visible_balls.header.frame_id)
+            new_dist = self.pose_dist(current_location, new_pose)
+
+            if new_dist < shortest_dist:
+                shortest_dist = new_dist 
+                closest_pose = new_pose
+
+        return closest_pose
+            
+
+    def stop(self):
+        twist = Twist()
+        self.velocity_pub.publish(twist)
+
+    def wait_for_stop(self):
+        rospy.loginfo("waiting for robot to stop")
+        while not rospy.is_shutdown() and not abs(self.odom.twist.twist.linear.x) < 0.02 and not abs(self.odom.twist.twist.angular.z) < 0.02:
+            rospy.sleep(0.1)
+        rospy.loginfo("Robot has stopped")
+        rospy.sleep(0.5)
 
 
     def run(self):
         self.generate_patrol()
         while not rospy.is_shutdown():
+
+            if self.saved_position is not None:
+                self.go_to_saved()
+                self.move_base_as.wait_for_result()
             self.schedule_patrol()
 
             while not rospy.is_shutdown() and not self.balls_full:
                 if self.visible_balls is not None and len(self.visible_balls.poses) > 0:
-                    self.save_position()
-                    self.cancel_all_move_goals()
-                    while not rospy.is_shutdown() and not self.balls_full and len(self.visible_balls.poses) > 0:
-                        seeking_pose = self.visible_balls.poses[0]
 
-                        ball_pose = self.generate_ball_pose(seeking_pose, self.visible_balls.header.frame_id)
+                    self.cancel_all_move_goals()
+                    self.stop()
+                    self.wait_for_stop()
+                    self.save_position()
+
+                    rospy.loginfo("Turning vacuum on")
+                    self.set_vacuum_state(True)
+
+                    while not rospy.is_shutdown() and not self.balls_full and len(self.visible_balls.poses) > 0:
+                        # seeking_pose = self.visible_balls.poses[0]
+
+                        # ball_pose = self.generate_ball_pose(seeking_pose, self.visible_balls.header.frame_id)
+                        ball_pose = self.get_closest_ball()
+                        rotation_pose = self.get_current_position()
+                        rotation_pose.orientation = ball_pose.orientation
+                        # rotation_pose.position = self.o
+
+                        self.add_move_goal(rotation_pose, False)
+                        self.move_base_as.wait_for_result()
+                        self.seek_path_done = False
+                        self.wait_for_stop()
                         rospy.loginfo("Adding seek move goal")
                         self.add_move_goal(ball_pose, False)
-                        
-                        rospy.loginfo("Turning vacuum on")
-                        self.set_vacuum_state(True)
+                        # self.seek_path_done = False
 
                         balls_before = self.balls_collected
                         rospy.loginfo(f"Balls before: {balls_before}")
@@ -282,28 +345,39 @@ class CollectNode:
                             rospy.loginfo(f"Balls now: {self.balls_collected}")
                             rospy.sleep(0.1)
                         self.seek_path_done = False
-                        rospy.loginfo("Turning vacuum off")
-                        self.set_vacuum_state(False)
                         self.cancel_all_move_goals()
+                        self.wait_for_stop()
+
+                    rospy.loginfo("Turning vacuum off")
+                    self.set_vacuum_state(False)
                     self.go_to_saved()
                     self.move_base_as.wait_for_result()
-                    self.schedule_patrol()
+                    if not self.balls_full:
+                        self.schedule_patrol()
 
                 rospy.sleep(0.1) 
-
+            
+            self.stop()
+            self.wait_for_stop()
             self.save_position()
             self.cancel_all_move_goals() 
 
             # go home 
-            self.add_move_goal(self.home_coords)
-            # wait until home
+            rospy.loginfo("Going to dump")
+            self.add_move_goal(self.home_coords, False)
             self.move_base_as.wait_for_result()
+            self.seek_path_done = False
+            # wait until home
 
             # dump balls 
             rospy.loginfo("Dumping...")
             self.total_balls_collected += self.balls_collected
-            dump_goal = DumpActionGoal(rospy.Duration(3.0))
-            self.dumper_as.send_goal_and_wait(dump_goal)
+            dump_goal = DumpActionGoal()
+            dump_goal.goal.dur = rospy.Duration(3.0)
+            dump_goal.goal_id.id = f"dump_{self.dumps}"
+            self.dumps += 1 
+            dump_goal.goal_id.stamp = rospy.Time.now()
+            self.dumper_as.send_goal_and_wait(dump_goal.goal)
             rospy.loginfo("Dumping done")
             rospy.sleep(2.0)
 
